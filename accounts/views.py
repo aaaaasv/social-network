e09ce0be038ad03235f9d969a6986c06c4f9c514
aaaaasv <00000001xyz@gmail.com>
2021-05-26 17:59:30 +0300

    Add user creation, list, login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class CreateUserPermission(permissions.IsAdminUser):
+
+    def has_permission(self, request, view):
+        if request.method in ['POST']:
+            return True
+
+        return super(CreateUserPermission, self).has_permission(request, view)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [CreateUserPermission]
