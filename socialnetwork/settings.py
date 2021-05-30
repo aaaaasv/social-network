23fbb053aaeffeac560804de55654a34848a2854
aaaaasv <00000001xyz@gmail.com>
2021-05-30 21:26:30 +0300

    Add root for static and login redirect url

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -138,3 +139,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+LOGIN_REDIRECT_URL = '/'
